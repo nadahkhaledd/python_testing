1628052a350ab2d5daa0cc0bc3a9a994ebdbf44d
nadahkhaledd <nadahkhaledd@gmail.com>
2022-08-28 23:34:55 +0200

    added tests for menu

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+MENU_PROMPT = 'Enter "c" to create a post, '\
+                      '"l" to list blogs, '\
+                      '"r" to read one, '\
+                      '"p" to create a post '\
+                      'or "q" to quit.: '
 blogs = dict()
 
 
 def menu():
     print_blogs()
+    selection = input(MENU_PROMPT)
+
 
 
 def print_blogs():
--- tests/system/test_app.py
+++ tests/system/test_app.py
@@ -4,9 +4,21 @@ import app
 from blog import Blog
 
 class AppTest(TestCase):
+    def test_menu_prompt_print(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+    def test_menu_calls_print_blog(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input', return_value='q'):
+                app.menu()
+                mocked_print_blogs.assert_called()
+
     def test_print_blog(self):
         b = Blog('Test', 'test author')
         app.blogs = {'Test': b}
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('- Test by test author (0 posts)')
+
